028cdd0370939ff6b21e0c1a130bf97b4ded3d7c
Amr <amrdoe@outlook.com>
2024-01-03 17:20:23 +0200

    Add home page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,29 @@
+import Navbar from "@/components/Navbar";
+import Hero from "@/components/Hero";
+import About from "@/components/About";
+import Experience from "@/components/Experience";
+import Tech from "@/components/Tech";
+import Works from "@/components/Works";
+import Feedbacks from "@/components/Feedbacks";
+import Contact from "@/components/Contact";
+import StarsCanvas from "@/components/canvas/Stars";
+
 export default function Home() {
-  return <main>3D Developer Portfolio</main>;
+  return (
+    <main className="relative z-0 bg-primary">
+      <header className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+        <Navbar />
+        <Hero />
+      </header>
+      <About />
+      <Experience />
+      <Tech />
+      <Works />
+      <Feedbacks />
+      <footer className="relative z-0">
+        <Contact />
+        <StarsCanvas />
+      </footer>
+    </main>
+  );
 }
