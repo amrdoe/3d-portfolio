7868742f3990e80d8d88545e4b99d5a2f2a53fb9
Amr <amrdoe@outlook.com>
2024-01-08 01:13:37 +0200

    Add Hero section

--- components/canvas/Computers.tsx
+++ components/canvas/Computers.tsx
@@ -1,9 +1,57 @@
-import React from 'react'
+"use client";
+
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
+import CanvasLoader from "../Loader";
+import useIsMobile from "@/lib/useIsMobile";
 
 const Computers = () => {
-  return (
-    <div>Computers</div>
-  )
-}
+  const computer = useGLTF("./desktop_pc/scene.gltf");
+  const isMobile = useIsMobile();
 
-export default Computers
+  return (
+    <mesh>
+      <hemisphereLight intensity={1} groundColor="black" />
+      <pointLight intensity={1} />
+      <spotLight
+        position={[-3, 10, 4]}
+        angle={0.12}
+        penumbra={1}
+        intensity={300}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <primitive
+        object={computer.scene}
+        scale={isMobile ? 0.7 : 0.75}
+        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
+      />
+    </mesh>
+  );
+};
+
+const ComputerCanvas = () => {
+  return (
+    <Canvas
+      frameloop="demand"
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers />
+      </Suspense>
+
+      <Preload all />
+    </Canvas>
+  );
+};
+
+export default ComputerCanvas;
