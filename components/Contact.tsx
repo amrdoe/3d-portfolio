99419a6225013d479275e49433e33aa14c7038b1
Amr <amrdoe@outlook.com>
2024-01-08 09:41:28 +0200

    Add Contact section

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,9 +1,86 @@
-import React from 'react'
+"use client";
+
+import { FormEvent, useRef, useState } from "react";
+import Section from "./Section";
+import { motion } from "framer-motion";
+import { slideIn } from "@/lib/motion";
+import styles from "@/lib/styles";
+import { EarthCanvas } from ".";
 
 const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
-}
+  const formRef = useRef<HTMLFormElement>(null);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [loading, setLoading] = useState(false);
 
-export default Contact
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
+  return (
+    <Section>
+      <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
+        <motion.div
+          variants={slideIn("left", "tween", 0.2, 1)}
+          className="flex-[0.75] bg-black-100 p-8 rounded-2xl"
+        >
+          <p className={styles.sectionSubText}>Get in touch</p>
+          <h3 className={styles.sectionHeadText}>Contact.</h3>
+
+          <form
+            ref={formRef}
+            onSubmit={handleSubmit}
+            className="mt-12 flex flex-col gap-8"
+          >
+            <label className="flex flex-col">
+              <span className="text-white font-medium mb-4">Your Name</span>
+              <input
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                placeholder="What's your name?"
+                className=" bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
+              />
+            </label>
+
+            <label className="flex flex-col">
+              <span className="text-white font-medium mb-4">Your Email</span>
+              <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="What's your email?"
+                className=" bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
+              />
+            </label>
+
+            <label className="flex flex-col">
+              <span className="text-white font-medium mb-4">Your Message</span>
+              <textarea
+                rows={7}
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                placeholder="What do you want to say?"
+                className=" bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
+              />
+            </label>
+
+            <button
+              type="submit"
+              className="bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl"
+            >
+              {loading ? "Sending..." : "Send"}
+            </button>
+          </form>
+        </motion.div>
+
+        <motion.div variants={slideIn("right", "tween", 0.2, 1)} className="xl:flex-1 xl:height-auto md:h-[550px] h-[350px]">
+          <EarthCanvas />
+        </motion.div>
+      </div>
+    </Section>
+  );
+};
+
+export default Contact;
--- components/canvas/Earth.tsx
+++ components/canvas/Earth.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { useGLTF } from "@react-three/drei";
+import Canvas from "./Canvas";
 
-const Earth = () => {
+const EarthCanvas = () => {
+  const earth = useGLTF("./planet/scene.gltf");
+
   return (
-    <div>Earth</div>
-  )
-}
+    <Canvas
+      shadows
+      camera={{ fov: 45, near: 0.1, far: 200, position: [-4, 3, 6] }}
+      orbitControls={{
+        autoRotate: true,
+        maxPolarAngle: Math.PI / 2,
+        minPolarAngle: Math.PI / 2,
+      }}
+    >
+      <primitive
+        object={earth.scene}
+        scale={2.5}
+        position-y={0}
+        rotation-y={0}
+      ></primitive>
+    </Canvas>
+  );
+};
 
-export default Earth
+export default EarthCanvas;
